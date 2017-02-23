cb728fd3582e5b5d75f86f754c68c10ba3ae0acc
rsgc-noblecurveira-c <student@macbook-pro-carlos-noble-curviera.local>
2017-02-22 20:11:18 -0500

    cannot seem to get boarders working so no progress on having a starting scene, some Progress on #3

--- Sandbox/Sandbox/GameScene.swift
+++ Sandbox/Sandbox/GameScene.swift
@@ -1,72 +1,88 @@
-//
-//  GameScene.swift
-//  Sandbox
-//
-//  Created by Carlos Noble Curveira on 2017-02-13.
-//  Copyright © 2017 Carlos Noble Curveira. All rights reserved.
-//
-
-import SpriteKit
-import GameplayKit
-
-class GameScene: SKScene {
+ //
+ //  GameScene.swift
+ //  Sandbox
+ //
+ //  Created by Carlos Noble Curveira on 2017-02-13.
+ //  Copyright © 2017 Carlos Noble Curveira. All rights reserved.
+ //
+ 
+ import SpriteKit
+ import GameplayKit
+ 
+ class GameScene: SKScene {
     
-    private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    private var circle : SKSpriteNode?
     
-    override func didMove(to view: SKView) {
+    
+    //    let width: CGFloat = frame
+    
+    override func didMove(to view: SKView)
+    {
         
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        //        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        //        if let label = self.label {
+        //            label.alpha = 0.0
+        //            label.run(SKAction.fadeIn(withDuration: 2.0))
+        //        }
+        
+        //        let w = (self.size.width + self.size.height) * 0.05
+        
+        
+        self.circle = SKSpriteNode.init(imageNamed: "circle.png")
+        
+        if let circle = self.circle
+        {
+            circle.run(SKAction.sequence([SKAction.wait(forDuration: 3),
+                                          SKAction.removeFromParent()]))
         }
         
         // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        //        let w = (self.size.width + self.size.height) * 0.05
+        //        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        //
+        //        if let spinnyNode = self.spinnyNode {
+        //            spinnyNode.lineWidth = 2.5
+        //
+        //            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
+        //            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+        //                                              SKAction.fadeOut(withDuration: 0.5),
+        //                                              SKAction.removeFromParent()]))
+        //        }
     }
     
+    func createSceneContents() {
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        
+    }
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        //        if let n = self.circle?.copy() as! SKSpriteNode? {
+        //            n.position = pos
+        //        }
+        circle?.position = pos
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        //        if let n = self.circle?.copy() as! SKSpriteNode? {
+        //            n.position = pos
+        //        }
+        circle?.position = pos
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+        if let n = self.circle?.copy() as! SKSpriteNode? {
             n.position = pos
-            n.strokeColor = SKColor.red
             self.addChild(n)
+            n.physicsBody = SKPhysicsBody(circleOfRadius: (circle?.size.width)! / 2)
+            
         }
     }
     
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
@@ -86,4 +102,4 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
-}
+ }
